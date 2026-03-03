8817c13fd7d4c29b62702471560b8442ded71940
thiruvenkatasamyiyya <thiruvenkatasamyiyya@gmail.com>
2026-03-03 20:13:15 +0530

    admin ui upated

---
+++ frontend/src/components/AdminUpload.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+const AdminUpload = () => {
+  const [file, setFile] = useState(null);
+  const [msg, setMsg] = useState("");
+
+  const uploadPDF = async () => {
+    if (!file) {
+      alert("Please select a PDF");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("pdf", file);
+
+    const res = await fetch("http://localhost:3000/api/v1/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await res.json();
+    setMsg(data.message);
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="p-4 border-b bg-white shadow-sm">
+        <h2 className="text-2xl font-bold text-gray-800">OpsMind AI</h2>
+      </div>
+
+      <div className="max-w-xl mx-auto mt-10 bg-white p-8 rounded-xl shadow-md">
+        <h2 className="text-xl font-semibold mb-6 text-gray-700">
+          Admin - Upload SOP PDF
+        </h2>
+
+        <label className="flex flex-col items-center justify-center w-full h-56 border-2 border-dashed border-gray-300 rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 transition">
+          <input
+            type="file"
+            accept="application/pdf"
+            className="hidden"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+
+          <div className="text-center px-4">
+            <p className="text-gray-600 text-lg font-medium">
+              {file ? file.name : "Click to upload PDF"}
+            </p>
+            <p className="text-sm text-gray-400 mt-2">
+              Only PDF files are allowed
+            </p>
+          </div>
+        </label>
+
+        
+        <button
+          onClick={uploadPDF}
+          className="w-full mt-6 bg-blue-600 hover:bg-blue-700 text-white font-medium py-2.5 rounded-lg transition"
+        >
+          Upload
+        </button>
+
+        {msg && (
+          <p className="mt-4 text-center text-green-600 font-medium">
+            {msg}
+          </p>
+        )}
+      </div>
+      
+    </div>
+  );
+};
+
+export default AdminUpload;
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 const SideBar = () => {
+  const navigate = useNavigate();
   return (
     <div className=" bg-blue-900 h-screen p-4 flex flex-col justify-between text-white">
-        <div>
-            <h1 className=' text-2xl font-bold '>SOP Agent</h1>
-            <hr className='border-white my-5' />
-        </div>
-        <div>
-            <h1>LogIn</h1>
+      <div>
+        <h1 className=" text-2xl font-bold ">SOP Agent</h1>
+        <hr className="border-white my-5" />
+        <div className="flex flex-col gap-3">
+          <div className="bg-blue-500 px-5 py-2 rounded-md" onClick={()=>navigate('/')}>
+            <span className="font-bold">Chat</span>
+          </div>
+          <div className="bg-blue-500 px-5 py-2 rounded-md" onClick={()=>navigate('/admin')}>
+            <span className="font-bold">Admin Control</span>
+          </div>
         </div>
       </div>
-  )
-}
+      <div>
+        <h1>LogIn</h1>
+      </div>
+    </div>
+  );
+};
 
-export default SideBar
+export default SideBar;
