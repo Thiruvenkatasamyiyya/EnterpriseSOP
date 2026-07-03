4296d22557dea37e65f59ebaa1cbe32571eff595
thiruvenkatasamyiyya <thiruvenkatasamyiyya@gmail.com>
2026-07-03 19:58:47 +0530

    logged out fix

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -74,11 +74,12 @@ export const loginUser = catchAsyncErrors(async(req, res, next) => {
 // Logout user => api/v1/logout
 export const logout = catchAsyncErrors(async(req, res, next) => {
 
-    res.cookie("token", null, {
-        expires: new Date(Date.now()),
-        httpOnly:true,
-        secure : true
-    })
+res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None", // if this is how it was originally set
+    path: "/",
+});
 
     res.status(200).json({
         message:"Logged out"
