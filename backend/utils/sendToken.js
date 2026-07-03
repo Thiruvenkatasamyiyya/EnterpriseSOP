7e36efe2f9435d7661cb70f48f77d6d0e0f7d4c8
thiruvenkatasamyiyya <thiruvenkatasamyiyya@gmail.com>
2026-07-03 20:06:47 +0530

    logged out fix

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -9,9 +9,10 @@ export default (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
-        httpOnly:true,
+        httpOnly: true,
         // secure:false, // development
-        secure : true,
+        secure: true,
+        sameSite: "None",
 
     };
 
